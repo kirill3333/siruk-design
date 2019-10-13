4c420a886ce2d5198840db679a88d0fdc46cabda
Kirill Novikov <kirill3333@inbox.ru>
2019-10-13 19:17:43 +0300

    svg logo component

---
+++ src/components/logo/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Icon from "../../assets/main-logo-white.svg";
+
+const Logo = () => {
+    return <div className="logo">
+        <Icon />
+    </div>
+}
+
+export default Logo
