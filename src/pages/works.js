96537a12e3a9ea1e0cb3cffffeb36b69d5fe9041
Kirill Novikov <kirill3333@inbox.ru>
2019-11-14 23:51:53 +0300

    add empty works page

---
+++ src/pages/works.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Layout from '../components/layout/default'
+import Wrapper from '../components/wrapper'
+import Header from '../components/header'
+
+const Works = () => {
+    return (
+      <Layout>
+          <Wrapper>
+              <Header isBlack />
+          </Wrapper>
+      </Layout>
+    )
+}
+
+export default Works
